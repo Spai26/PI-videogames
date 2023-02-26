26cc2f35d5cef73c04d67b42ad070652a297cc5f
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-02-25 19:45:50 -0500

    confing files css

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
 /* import routes */
 import Detail from "./information/videogames/pages/Details/Detail";
+import Landing from "./information/components/Pages/Landing/Landing";
 import Home from "./information/components/Pages/Home/Home";
+import Error404 from "./information/components/Pages/Error/Error404";
 import Navigation from "./information/components/Partials/Navigation/Nav";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useLocation,
+} from "react-router-dom";
 
 /* import styles */
 import "./information/css/app.css";
 function App() {
+  const location = useLocation();
   return (
     <Router>
       <div className="App">
-        <Navigation />
-        <div className="hola">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/detail">
-              <Detail />
-            </Route>
-          </Switch>
-        </div>
+        {location.pathname !== "/" ? <Navigation /> : ""}
+
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/detail" component={Detail} />
+          <Route path="*" component={Error404} />
+        </Switch>
       </div>
     </Router>
   );
