38276cf5a69015677faf2fd105a8bbb339d01262
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-02-25 19:49:10 -0500

    add file styles

--- client/src/information/components/Pages/Home/Home.jsx
+++ client/src/information/components/Pages/Home/Home.jsx
@@ -1,15 +1,6 @@
+import styles from "./Home.module.css";
 const Home = () => {
-  return (
-    <div>
-      my page home
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-        consequuntur, sit laboriosam dicta nam quisquam aliquid natus nulla non
-        officiis vel nihil maiores! Explicabo neque voluptatum deleniti
-        molestiae, eius odio?
-      </p>
-    </div>
-  );
+  return <div className={styles.start}></div>;
 };
 
 export default Home;
