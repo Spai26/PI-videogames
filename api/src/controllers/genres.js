646fa500562bf8d6ae3a4214bbb593768f975c8f
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-03-02 10:12:44 -0500

    confirm model

---
+++ api/src/controllers/genres.js
@@ -0,0 +1,30 @@
+const { URL_BASE, GENDER, API_KEY, Genre } = require("../db");
+const axios = require("axios");
+
+const getGenres = async (req, res, next) => {
+  try {
+    const apiGenres = await axios.get(`${URL_BASE}${GENDER}?key=${API_KEY}`, {
+      responseType: "json",
+    });
+
+    const genresApi = apiGenres.data.results;
+    genresApi.map(async (g) => {
+      await Genre.findOrCreate({
+        where: { name: g.name },
+        defaults: {
+          id: g.id,
+          name: g.name,
+        },
+      });
+    });
+    res.json({ data: genresApi });
+  } catch (e) {
+    (e) => {
+      next(e);
+    };
+  }
+};
+
+module.exports = {
+  getGenres,
+};
