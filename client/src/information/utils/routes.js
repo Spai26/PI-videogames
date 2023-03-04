33026abb93226f637258659c5cef3904265270e3
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-03-04 18:57:27 -0500

    generate route from backend

--- client/src/information/utils/routes.js
+++ client/src/information/utils/routes.js
@@ -1,12 +1,14 @@
 /**
- * !TODO : ruta local de mi backend
+ * !TODO: ruta local de mi backend
  * @param URL_HV
  * @param URL_HOME         [llama todos los videogame]
+ * @param URL_GENDERS
  * @param URL_ADD_VGAME    [crear videogame]
  * @param URL_VGAME_DETAIL [detalle videogame]
- * !FIXME URL_SEARCH
+ * !FIXME: URL_SEARCH
  */
 export const URL_HV = "http://localhost:3001";
 export const URL_HOME = URL_HV + "/videogames";
 export const URL_ADD_VGAME = URL_HV + "/videogames";
 export const URL_VGAME_DETAIL = URL_HV + "/videogames";
+export const URL_GENDERS = URL_HV + "/genres";
