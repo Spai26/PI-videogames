cfb4cfda5c6bb2333979ef6c828927cabc50734d
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-03-04 23:48:31 -0500

    add route icons

--- client/src/information/utils/utils.js
+++ client/src/information/utils/utils.js
@@ -1,14 +1,23 @@
 /* images from landing page */
 
 /* icons from  search*/
-import isearch from "../assets/icons/search.png";
-/* icons from navigation  */
-import icross from "../assets/icons/cross.png";
-import ihamburger from "../assets/icons/hamburger.png";
-
+import ihamburger from "../assets/icons/hamburger.svg";
+import isearch from "../assets/icons/search.svg";
+import imenufilter from "../assets/icons/dashboard.svg";
+import icross from "../assets/icons/icons8-multiply-50.svg";
+import idata from "../assets/icons/icons8-base-32.svg";
+import logo from "../assets/icons/gamer.png";
+import inext from "../assets/icons/next_1.svg";
+import ileft from "../assets/icons/left.svg";
+import iadd from "../assets/icons/icons8-add-48.svg";
 export const icons = {
+  logo: logo,
   hamburger: ihamburger,
   cross: icross,
   search: isearch,
+  filter: imenufilter,
+  bd: idata,
+  next: inext,
+  after: ileft,
+  add: iadd,
 };
-
