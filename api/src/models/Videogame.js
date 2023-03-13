cb9dc63df93b1614ba132d59528665560ee88391
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-03-13 18:32:56 -0500

    platforms attribute

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -17,7 +17,7 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    platform: {
+    platforms: {
       type: DataTypes.STRING,
       allowNull: false,
     },
